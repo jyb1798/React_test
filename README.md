43a198dc855b77defa0bf2fd95822c31dee855bf
jyb8509 <bestdung8509@gmail.com>
2022-01-17 17:19:41 +0900

    react test project

--- README.md
+++ README.md
@@ -1,17 +1,1 @@
-# Project | wanted_pre_onboarding
-
-## [위코드 X 원티드] 프론트앤드 프리온보딩 선발 과제
-
-1. 프로젝트 목표 : 원티드 페이지 상단 영역을 React 기반으로 클론
-
-2. 기간 : 1/15 ~ 1/19
-
-3. 필수 구현사항 및 진행률
-
-   - 상단 GNB(Global Navigation Bar) 마크업 : 1/15일 %
-   - 슬라이드(또는 캐러샐이라고 불림) : 1/15일 %
-   - 반응형 : 1/15일 %
-   - netlify로 배포 : 1/15일 %
-
-4. 적용 기술
-   - React, Styled-Component
+# Project | react test project
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,11 +5,21 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <Header>
-      <Inner>
-        <Link to="/"></Link>
-      </Inner>
+      <Inner>test Nav 입니다</Inner>
     </Header>
   );
 };
 
 export default Nav;
+
+const Header = styled.div`
+  background-color: white;
+  border-bottom: solid 2px #e8e8e8;
+`;
+
+const Inner = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
