b3441c80dadf9759218a813d3853ab87d97f73e8
jyb8509 <bestdung8509@gmail.com>
2022-01-17 15:24:55 +0900

    router & styled-components 세팅

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <Header>
+      <Inner>
+        <Link to="/"></Link>
+      </Inner>
+    </Header>
+  );
+};
+
+export default Nav;
